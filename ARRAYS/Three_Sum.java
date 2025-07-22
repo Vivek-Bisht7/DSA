73c260b82cf21d2c41c0ad38b3c4e3b5c4ea58cd
Vivek <vivekbisht4320@gmail.com>
2025-07-22 22:11:26 +0530

    Solved

--- ARRAYS/Three_Sum.java
+++ ARRAYS/Three_Sum.java
@@ -59,4 +59,39 @@ class Solution {
     }
 }
 
-// Not further optimised yet
+// optimised = Two points + sorting
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            int j = i + 1, k = nums.length - 1;
+
+            while (j < k) {
+                int value = nums[i] + nums[j] + nums[k];
+
+                if (value == 0) {
+                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                    while (j < k && nums[j] == nums[j - 1])
+                        j++;
+                    while (j < k && nums[k] == nums[k + 1])
+                        k--;
+                } else if (value > 0) {
+                    k--;
+                } else {
+                    j++;
+                }
+
+            }
+
+        }
+        return ans;
+
+    }
+}
