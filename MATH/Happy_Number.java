06910628bf9ded64d4fa157d989739f17ee9be84
Vivek <vivekbisht4320@gmail.com>
2025-09-04 21:20:11 +0530

    Solved

---
+++ MATH/Happy_Number.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isHappy(int n) {
+        HashSet<Integer> set = new HashSet<>();
+
+        while(true){
+            int sum = 0;
+
+            while(n>0){
+                int rem = n%10;
+
+                sum += rem*rem;
+
+                n /= 10;
+            }
+
+            if(sum==1) return true;
+            else{
+                if(set.contains(sum)) return false;
+                else set.add(sum);
+            }
+            n = sum;
+        }
+    }
+}
