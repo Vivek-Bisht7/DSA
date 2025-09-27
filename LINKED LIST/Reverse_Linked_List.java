8a55988c9b1c1ca824b98c4d59f07027b45b3eb8
Vivek <vivekbisht4320@gmail.com>
2025-09-27 22:49:29 +0530

    Solved

--- LINKED LIST/Reverse_Linked_List.java
+++ LINKED LIST/Reverse_Linked_List.java
@@ -1,3 +1,5 @@
+// Iterative
+
 
 class Solution {
     public ListNode reverseList(ListNode head) {
@@ -22,5 +24,21 @@ class Solution {
         }
 
         return previous;
+    }
+}
+
+// Recursive
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        
+        if(head==null || head.next==null) return head;
+
+        ListNode newHead = reverseList(head.next);
+        ListNode temp = head.next;
+        temp.next = head;
+        head.next = null;
+
+        return newHead;
     }
 }
